9b5300f4a905f11268c963862530742e888fcd61
Sergey Nikiforov <ser.nikiforov@gmail.com>
2017-09-04 14:19:54 +0400

    service restart fix

--- install.sh
+++ install.sh
@@ -8,11 +8,14 @@ PHPMODDIR=/usr/lib/php/20131226/
 
 # подключаем модуль к PHP 
 cp ./modules/sweph.so $PHPMODDIR
+echo ">>> Extentions file has been copied to modules directory\n"
 touch $PHPDIR/mods-available/sweph.ini
 cat >$PHPDIR/mods-available/sweph.ini <<EOF
 extension=sweph.so
 EOF
 ln -s $PHPDIR/mods-available/sweph.ini $PHPDIR/fpm/conf.d/20-sweph.ini
 ln -s $PHPDIR/mods-available/sweph.ini $PHPDIR/cli/conf.d/20-sweph.ini
+echo ">>> Ini files has been created for PHP\n"
 service php5.6-fpm restart
+echo ">>> PHP-fpm service has been restarted\n"
 
