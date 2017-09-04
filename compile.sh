13511857df30c5a8512ca9e148e23b112399c77c
Sergey Nikiforov <ser.nikiforov@gmail.com>
2017-09-04 14:23:18 +0400

    service restart fix

--- compile.sh
+++ compile.sh
@@ -14,15 +14,21 @@ INCLUDEPATH=/usr/local/include
 # компилируем библиотеку SWEPH 
 wget $SWEURL
 tar -xzvf $(basename $SWEURL)
+echo "\n>>> Swiss Ephemeris library has been downloaded\n"
 cd src
 make
+echo "\n>>> Swiss Ephemeris library has been compiled\n"
 cd ..
 cp ./src/libswe.a $LIBPATH
 cp ./src/sweodef.h $INCLUDEPATH
 cp ./src/swephexp.h $INCLUDEPATH
+echo "\n>>> Swiss Ephemeris library files has been placed to default paths\n"
 
 # компилируем модуль для PHP7 
 phpize
+echo "\n>>> sweph-php extention has been phpized\n"
 ./configure  --enable-sweph
+echo "\n>>> sweph-php extention has been configured\n"
 make
+echo "\n>>> sweph-php extention has been successfully created\nPlease run sudo ./install.sh\n"
 
